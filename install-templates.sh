47bbad67a0fd45a8ffdcf9120f97449179b16457
Alejandro Ramirez <jano@jano.com.es>
2011-07-21 15:27:43 +0200

    fix: create initial directories

--- install-templates.sh
+++ install-templates.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir -p ~/Library/Developer/Xcode/Templates/Project\ Templates/
+mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/
 cp -vr templates/Project\ Templates/ ~/Library/Developer/Xcode/Templates/Project\ Templates/
 cp -vr templates/File\ Templates/ ~/Library/Developer/Xcode/Templates/File\ Templates/
