047534741963dd716d2469d2a5b0dff00501c1d3
Alejandro Ramirez <alejandro@madpixel.es>
2011-07-22 23:21:51 +0200

    safe release macro

--- templates/File Templates/Jano/Objective-C class.xctemplate/NSObject/___FILEBASENAME___.h
+++ templates/File Templates/Jano/Objective-C class.xctemplate/NSObject/___FILEBASENAME___.h
@@ -2,8 +2,6 @@
 //  Created by ___FULLUSERNAME___.
 //  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 
-#import <Foundation/Foundation.h>
-
 @interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
 
 @end
